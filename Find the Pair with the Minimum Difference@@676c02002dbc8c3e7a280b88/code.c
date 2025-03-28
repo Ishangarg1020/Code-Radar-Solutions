860fc7886cc6bae4dc9ff52cb1236d812e722bc6
Ishan Garg <gargishan0001@gmail.com>
2025-03-28 10:22:47 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -48,7 +48,14 @@
 
             }
         }
-        printf("%d %d",a,b);
+        if(a<b)
+        {
+            printf("%d %d",a,b);
+        }
+        else
+        {
+            printf("%d %d",b,a);
+        }
     }
     
  }
